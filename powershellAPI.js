4d946a15d357e57adf227f1ec64de7152a1e0287
Darren <darrn@users.noreply.github.com>
2014-10-13 15:23:04 -0700

    rewrite

--- powershellAPI.js
+++ powershellAPI.js
@@ -1,364 +1,142 @@
 var restify = require('restify');
-var exec = require('child_process').exec;
-var fs = require('fs');
-//removed edge, found simple solution
+var process = require('child_process');
+//var fs = require('fs'); We shouldn't be opening files directly?
+//all counting of anything starts at zero
 
-
-/* status: no error handling, need to clean up code*/
-
-
-
-//change these as needed
-var sessionTotal = 0;
-var workingFolderPath = 'TEMP';
-var macAddress = ["test1", "test2"];
-console.log(macAddress[0]);
-
-//powershell('DTTSD702207W764', 'shutdown -r -t 60; shutdown -a');
-
-//removed powershellLocal, just use localhost as the host
-
-
-
-
-console.log('powershellAPI init on ' + Date());
-
-//Kill off the working folder (and everything in it) to remove leavings from the last session
-//fs.rmdir(workingFolderPath);
-//fs.mkdir(workingFolderPath);
-
-
-function GET(req, res, next)
+function gui (req, res, next)
 {
-	res.writeHead(200, 
+	res.writeHead(200, { 'Content-Type': 'html/text'	});
+
+	switch (req.url)
 	{
-		'Content-Type': 'text/html'
-	});
-	
-	console.log('get: ' + req.url);
-	switch (req.url) 
-	{
-		case '/addmac':
-			res.end('NO');
-		next();
+		default:
+			res.end('');
 		break;
-		case '/':
-			res.end('neat gui should go here');
-		next();
-		break;
-		
 	};
 };
 
 
-function POST(req, res, next)
+function api (req, res, next)
 {
-	
-	
-	
-	//to quickly pop out JSON
-	function makeJSON(amount, total, variable, value)
-	{
-		if (amount != total && amount != total -1)
+	res.writeHead(200, { 'Content-Type': 'application/json'	});
+
+	function windowsSendcmd ()
+	{//Note: the computers will not return in the order that they were sent, but in the order of how quickly they ran.
+
+
+		// == figure out how many computers we have been sent == //
+
+		finalAmount = 0; //define before hand due to scoping
+
+		console.log('it ran');
+		for (var amount = 0; amount != -1; amount++)
 		{
-			res.write('{"' + variable + '":"' + value + '",');
+			//console.log(amount);
+			if (typeof req.body.computer[amount]=="undefined")
+			{
+				//console.log("It's undefined!");
+				finalAmount = (amount - 1);
+				break;
+			}
 		}
-		
-		if (amount == total - 1)
-		{
-			res.end('"' + variable + '":"' + value + '"}');
-		}
-	};
-	
-	
-	//AD mod
 
-	
-	function powershell(name, commands)
-	{
-		exec('powershell.exe -Command invoke-command -computername "' + name + ' -ScriptBlock {' + commands + '}"', function(err, stdout, stderr) 
-		{
-			console.log(stderr);
-			return(stderr.length);
-		})
-	};
-	
-	//init a powershell command and return it to the client (cuts the amount of code by four)
-	function sendAndReturnPS(name, host, commands, amount, total, special)
-	{
-		exec('powershell.exe -Command invoke-command -computername ' + host + ' -ScriptBlock {' + commands + '}"', function(err, stdout, stderr) 
-		{
-			switch(special)
-			{
-				case 'robocopy':
-					var result = stdout.substring(stdout.length - 36, stdout.length - 4);
-					break;
-				default:
-					if(stderr)
-					{
-						var result = 'ERROR';
-					}
-					if(stdout)
-					{
-						var result = 'SUCCESS';
-					}
-					break;
-			}
-			
-			
-			res.write('[');
-			if (amount != total && amount != total -1)
-			{
-				res.write('\
-				\t{\n\
-				"' + name + '": [\n\
-				"' + result + '"\n\
-				]\n\
-				},\n\
-				\
-				');
-			}
-			
-			if (amount == total - 1)
-			{
-				res.write('\
-				\t{\n\
-				"' + name + '": [\n\
-				"' + result + '"\n\
-				]\n\
-				}\n\
-				\
-				');
-				res.end(']');
-			}
-			console.log('Done with command ' + commands + ' on host ' + host + ' with name ' + name);
-			console.log(stderr + stdout);
-		})
-	};
-	
-	
-	//TODO: Add remote ssh support
-	
-	//end functions
-	
-	switch (req.url) 
-	{
-		case '/':
-			makeJSON(0, 5, '/network/macaddress', 'Inputs mac addresses into the school network. Vars must start with macAddress[0], and continue upwords.');
-			makeJSON(1, 5, '/remote/reboot', 'Reboots a selection of computers. Vars must start with computer[0] and continue upwords.');
-			makeJSON(2, 5, '/remote/mkdir', 'Creates an folder. Use the var path to specify where to create the folder. Vars must start with computer[0] and continue upwords.');
-			makeJSON(3, 5, '/remote/rmdir', 'Removes an folder. Use the var path to specify where to remove the folder. Vars must start with computer[0] and continue upwords.');
-			makeJSON(4, 5, '/remote/robocopy', 'Copys an folder or file. Use the var path[0] to specify where to copy the folder or file from, and path[1] for the destination. Vars must start with computer[0] and continue upwords.');
-			break;
-			
-		case '/remote/robocopy':
-		//TODO: Rewrite this entire switch
-			if (!req.body.computer[0])
-			{
-				res.end('computers must be defined as array');
-				break;
-			}
-			
-			if (!req.body.path[0])
-			{
-				res.end('path[0] folder to copy from');
-				break;
-			}
-			
-			if (!req.body.path[1])
-			{
-				res.end('path[1] folder to copy to');
-				break;
-			}
-			
-			
-			//Checks to see how many items have been sent
-			for (var amount = 0; amount > -1; amount++)
-			{
-				if (!req.body.computer[amount])
-				{
-					var totalComputers = amount;
-					sessionTotal = sessionTotal + amount;
-					break;
-				}
-			}
-			
-			for (var amount = 0; amount < totalComputers; amount++)
-			{
-				sendAndReturnPS(req.body.computer[amount], req.body.computer[amount], 'robocopy "' + req.body.path[0] + '" "' + req.body.path[1] + '" /E', amount, totalComputers, 'robocopy');
-			}
-			
-			break;
-		
-		case '/remote/mkdir':
-		
-			if (!req.body.computer[0])
-			{
-				makeJSON(0, 1, 'ERROR', 'Computers must be defined as array starting at 0');
-				break;
-			}
-			
-			if (!req.body.path)
-			{
-				makeJSON(0, 1, 'ERROR', 'Var path must be defined!');
-				break;
-			}
-			
-			
-			//Checks to see how many items have been sent
-			for (var amount = 0; amount > -1; amount++)
-			{
-				if (!req.body.computer[amount])
-				{
-					var totalComputers = amount;
-					sessionTotal = sessionTotal + amount;
-					break;
-				}
-			}
-			
-			for (var amount = 0; amount < totalComputers; amount++)
-			{
-				sendAndReturnPS(req.body.computer[amount], req.body.computer[amount], 'mkdir "' + req.body.path + '"', amount, totalComputers);
-			}
-			
-			break;
-			
-		case '/remote/rmdir':
-		
-			if (!req.body.computer[0])
-			{
-				makeJSON(0, 1, 'ERROR', 'Computers must be defined as array starting at 0');
-				break;
-			}
-			
-			if (!req.body.path)
-			{
-				makeJSON(0, 1, 'ERROR', 'Var path must be defined!');
-				break;
-			}
-			
-			
-			//Checks to see how many items have been sent
-			for (var amount = 0; amount > -1; amount++)
-			{
-				if (!req.body.computer[amount])
-				{
-					var totalComputers = amount;
-					sessionTotal = sessionTotal + amount;
-					break;
-				}
-			}
-			
-			for (var amount = 0; amount < totalComputers; amount++)
-			{
-				sendAndReturnPS(req.body.computer[amount], req.body.computer[amount], 'rmdir "' + req.body.path + '"', amount, totalComputers);
-			}
-			
-			break;
-		
-		
-		
-		
-		case '/remote/reboot':
-		
-			if (!req.body.computer[0])
-			{
-				makeJSON(0, 1, 'ERROR', 'Computers must be defined as array starting at 0');
-				break;
-			}
-			
-			
-			//Checks to see how many items have been sent
-			for (var amount = 0; amount > -1; amount++)
-			{
-				if (!req.body.computer[amount])
-				{
-					var totalComputers = amount;
-					sessionTotal = sessionTotal + amount;
-					break;
-				}
-			}
-			
-			for (var amount = 0; amount < totalComputers; amount++)
-			{
-				sendAndReturnPS(req.body.computer[amount], req.body.computer[amount], 'shutdown -r -t 0 "' + req.body.path + '"', amount, totalComputers);
-			}
-			
-			break;
-			
-			
-		case '/network/addmac':
-			
-			
-			
-				
+		// == now run the requested command on each computer we've been sent (will be resource _heavy_) ==//
 
-				//if (address != null) {
-				/*sendAndReturnPS(req.body.macAddress[amount], req.body.macAddress[amount], ', amount, totalMacAddresses)
-				{
-					
-				};
-				*/
-			
+		function prepareForCommands()
+		{
+			res.write('[{');
+			runCommands();
+		};
 
-			
-			break;
-			
+
+		function runCommands ()
+		{
+			for (var amount = 0; amount != (finalAmount + 1); amount++)
+			{
+				//console.log(amount + '/' + finalAmount);
+
+				/*if (amount == 0)
+				{
+					process.exec('powershell.exe -Command invoke-command -computername ' + req.body.computer[finalAmount] + ' -ScriptBlock {echo ' + req.body.computer[amount] + ';' + req.body.cmd + '}"', returnLastCommandResults);
+				}*/
+
+				//Run the last machine later on, not here inside this for loop
+				if (amount != finalAmount)
+				{
+					process.exec('powershell.exe -Command invoke-command -computername ' + req.body.computer[amount] + ' -ScriptBlock {echo ' + req.body.computer[amount] + ';' + req.body.cmd + '}"', returnCommandResults);
+				}
+			}
+		};
+
+		var totalTimesRan = 0; //outside due to scope
+		function countRanCommands ()
+		{
+			if (totalTimesRan == (finalAmount - 1))
+			{
+				//Running the last one here, everything else has already been ran
+				process.exec('powershell.exe -Command invoke-command -computername ' + req.body.computer[finalAmount] + ' -ScriptBlock {echo ' + req.body.computer[amount] + ';' + req.body.cmd + '}"', returnLastCommandResults);
+			}else
+			{
+				//This is not the last script to be ran...
+				totalTimesRan++;
+				//Not sure if this will work with a lot of commands ending at once...
+			}
+		};
+
+		// == now tell about the commands we ran ==//
+
+		function returnCommandResults (err, stdout, stderr)
+		{
+			console.log('not final');
+			res.write('"stdout": "' + stdout.replace( /\r?\n|\r/g, r) + '",');
+			res.write('"stderr": "' + stderr + '",');
+			res.write('"err": "' + err + '"');
+			countRanCommands(); // due to sync. running, the scripts will run utterly out of order, we need to count how many are done.
+		};
+
+		function returnLastCommandResults (err, stdout, stderr)
+		{
+			console.log('final');
+			res.write('"stdout": "' + stdout + '",');
+			res.write('"stderr": "' + stderr + '",');
+			res.write('"err": "' + err + '"');
+			endCommands(); //This is started after the one before the last has finished, therefore we know this will be the last one to start.
+		};
+
+
+
+		function endCommands ()
+		{
+			console.log('it ended');
+			res.end('}]');
+		};
+
+		prepareForCommands();
 	};
-	
-	
+
+	console.log(req.url);
+
+	switch (req.url)
+	{
+		default:
+			res.end('');
+		break;
+		case '/windows/sendcmd':
+			windowsSendcmd();
+		break;
+	};
+
 };
 
 var server = restify.createServer();
 server.use(restify.bodyParser({ mapParams: false }));
 
+server.get('/:everything/:anything', gui);
+server.post('/:everything/:anything', api);
 
-server.get('/', GET);
-server.post('/', POST);
+function ready()
+{
+	console.log('Ready for requests, on port 80.');
+};
 
-server.get('/network/addmac', GET);
-
-
-
-server.post('/network/addmac', function(req, res){
-
-	res.writeHead(200, 
-	{
-		'Content-Type': 'text',
-		'Access-Control-Allow-Origin': 'http://localhost:8080'
-	});
-
-	var macAddress = req.body;
-
-	for (var amount = 0; amount < macAddress.length; amount++){
-		if (macAddress[amount].valid) {
-			exec('powershell.exe C:/Users/serv_datascript/Desktop/New-MacAddress.ps1 ' + macAddress[amount].address, function(err, stdout, stderr) {
-				console.log(stdout);
-				macAddress[amount].added = true;
-				return(stderr.length);
-			});
-			res.end("Mac Address '" + macAddress.address + "' has been entered correctly.");
-		} else {
-			res.end("Invalid Mac Address Detected.");
-		}
-	}
-});
-
-
-
-
-server.get('/remote/reboot', GET);
-server.post('/remote/reboot', POST);
-
-server.get('/remote/rmdir', GET);
-server.post('/remote/rmdir', POST);
-
-server.get('/remote/mkdir', GET);
-server.post('/remote/mkdir', POST);
-
-server.get('/remote/robocopy', GET);
-server.post('/remote/robocopy', POST);
-
-server.listen(80, function() {
-  console.log('Listening on ' + server.url);
-});
+server.listen(80, ready);
