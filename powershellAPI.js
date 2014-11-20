18ff8c3746ec17b144471da6b09c6f62c7c10c48
Darren <darrn@users.noreply.github.com>
2014-11-19 16:37:35 -0800

    rewrite

--- powershellAPI.js
+++ powershellAPI.js
@@ -1,142 +1,106 @@
 var restify = require('restify');
 var process = require('child_process');
-//var fs = require('fs'); We shouldn't be opening files directly?
-//all counting of anything starts at zero
+//var fs = require('fs');
 
-function gui (req, res, next)
+
+
+function cleanInput(input, level)
 {
-	res.writeHead(200, { 'Content-Type': 'html/text'	});
-
-	switch (req.url)
+	//Clean input passed through via regular expression
+	switch (level)
 	{
 		default:
-			res.end('');
+			var output = input.replace(/([<])/g, '[');
+			var output = output.replace(/([>])/g, ']');
+			var output = output.replace(/(['])/g, '"');
 		break;
-	};
-};
+		case 'strict':
+			var output = input.replace(/[^A-Za-z0-9]/g, '');
+		break;
+		case 'email':
+			var output = input.replace(/[^A-Za-z0-9@_\u002e-]/g, '');
+		break;
+	}
+
+	return output;
+}
 
 
-function api (req, res, next)
+
+
+
+
+
+
+function api(req, res, next)
 {
-	res.writeHead(200, { 'Content-Type': 'application/json'	});
+	function sendCommand(command, computerList, callback)
+	{
+		//Input a command (string), and an array of computers.
 
-	function windowsSendcmd ()
-	{//Note: the computers will not return in the order that they were sent, but in the order of how quickly they ran.
-
-
-		// == figure out how many computers we have been sent == //
-
-		finalAmount = 0; //define before hand due to scoping
-
-		console.log('it ran');
-		for (var amount = 0; amount != -1; amount++)
+		for (var amt = 0; amt < computerList.length; amt++)
 		{
-			//console.log(amount);
-			if (typeof req.body.computer[amount]=="undefined")
+			runCommand(command, computerList[amt], computerList.length);
+		}
+
+		var computers = [];
+		function runCommand(command, computerName)
+		{
+			console.log(command);
+			process.exec('powershell.exe -Command invoke-command -computername ' + computerName + ' -ScriptBlock {' + command + '}"', addResultsToArray);
+
+
+			function addResultsToArray(error, stdout, stderr)
 			{
-				//console.log("It's undefined!");
-				finalAmount = (amount - 1);
-				break;
+				computers.push({"name": computerName, "error": error, "stdout": stdout, "stderr": stderr });
+				countResults();
 			}
 		}
 
-		// == now run the requested command on each computer we've been sent (will be resource _heavy_) ==//
-
-		function prepareForCommands()
+		totalAmount = 0;
+		function countResults()
 		{
-			res.write('[{');
-			runCommands();
-		};
-
-
-		function runCommands ()
-		{
-			for (var amount = 0; amount != (finalAmount + 1); amount++)
+			console.log(totalAmount);
+			totalAmount++;
+			if (totalAmount == computerList.length)
 			{
-				//console.log(amount + '/' + finalAmount);
-
-				/*if (amount == 0)
-				{
-					process.exec('powershell.exe -Command invoke-command -computername ' + req.body.computer[finalAmount] + ' -ScriptBlock {echo ' + req.body.computer[amount] + ';' + req.body.cmd + '}"', returnLastCommandResults);
-				}*/
-
-				//Run the last machine later on, not here inside this for loop
-				if (amount != finalAmount)
-				{
-					process.exec('powershell.exe -Command invoke-command -computername ' + req.body.computer[amount] + ' -ScriptBlock {echo ' + req.body.computer[amount] + ';' + req.body.cmd + '}"', returnCommandResults);
-				}
+				callback(computers);
 			}
-		};
+		}
+	}
 
-		var totalTimesRan = 0; //outside due to scope
-		function countRanCommands ()
-		{
-			if (totalTimesRan == (finalAmount - 1))
-			{
-				//Running the last one here, everything else has already been ran
-				process.exec('powershell.exe -Command invoke-command -computername ' + req.body.computer[finalAmount] + ' -ScriptBlock {echo ' + req.body.computer[amount] + ';' + req.body.cmd + '}"', returnLastCommandResults);
-			}else
-			{
-				//This is not the last script to be ran...
-				totalTimesRan++;
-				//Not sure if this will work with a lot of commands ending at once...
-			}
-		};
-
-		// == now tell about the commands we ran ==//
-
-		function returnCommandResults (err, stdout, stderr)
-		{
-			console.log('not final');
-			res.write('"stdout": "' + stdout.replace( /\r?\n|\r/g, r) + '",');
-			res.write('"stderr": "' + stderr + '",');
-			res.write('"err": "' + err + '"');
-			countRanCommands(); // due to sync. running, the scripts will run utterly out of order, we need to count how many are done.
-		};
-
-		function returnLastCommandResults (err, stdout, stderr)
-		{
-			console.log('final');
-			res.write('"stdout": "' + stdout + '",');
-			res.write('"stderr": "' + stderr + '",');
-			res.write('"err": "' + err + '"');
-			endCommands(); //This is started after the one before the last has finished, therefore we know this will be the last one to start.
-		};
-
-
-
-		function endCommands ()
-		{
-			console.log('it ended');
-			res.end('}]');
-		};
-
-		prepareForCommands();
-	};
-
-	console.log(req.url);
-
-	switch (req.url)
+	switch(req.url)
 	{
-		default:
-			res.end('');
-		break;
-		case '/windows/sendcmd':
-			windowsSendcmd();
-		break;
-	};
+		case '/powershell/sendcommand':
+			console.log(req.body);
+			sendCommand(req.body.command, req.body.computers, returnResults);
+			function returnResults(results)
+			{
+				res.end(JSON.stringify(results));
+			}
 
-};
+			break;
+		case '/':
+			console.log(req.body);
+			break;
+	}
+}
+
+
+
+
+
 
 var server = restify.createServer();
 server.use(restify.bodyParser({ mapParams: false }));
 
-server.get('/:everything/:anything', gui);
-server.post('/:everything/:anything', api);
+
+server.post('/:a', api);
+server.post('/:a/:a', api);
 
 function ready()
 {
 	console.log('Ready for requests, on port 80.');
-};
+}
 
 server.listen(80, ready);
